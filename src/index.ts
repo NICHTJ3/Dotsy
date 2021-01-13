47071dec9d57a27a9985ae21862e6cf0f75c998a
NICHTJ3 <nicholsontrent@gmail.com>
2021-01-13 13:27:36 +1300

    chore: Add command example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("It works")
+import { Command } from 'commander';
+
+const program = new Command();
+program.version('0.0.1');
+
+program
+  .command('profile [subcommand]')
+  .option('-i,--install', 'Install a profile')
+  .action(function (subcommand: string | undefined, cmdObj) {
+    console.log(subcommand + (cmdObj.install ? ' install' : 'not install'));
+  });
+
+program.parse(process.argv);
