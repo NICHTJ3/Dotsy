2e2db79deecae97553a526780ac02ce11829fc3c
Trent Nicholson <nicholsontrent@gmail.com>
2021-10-05 15:47:57 +1300

    init rust project

--- src/parsers/dotsyrc/validator.ts
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const globalConfig = z.object({
-  packages_install_cmd: z.string().default('sudo apt install'),
-  packages_uninstall_cmd: z.string().default('sudo apt remove'),
-});
-
-export type GlobalConfigType = z.infer<typeof globalConfig>;
--- src/plugins/link.ts
+++
@@ -1,7 +0,0 @@
-import { existsSync } from 'fs';
-import fs from 'fs/promises';
-
-export default async function link(from: string, to: string) {
-  // TODO: Should I check they exist? check dotbot for more info
-  // TODO: Should it be a symlink of a hard link
-}
