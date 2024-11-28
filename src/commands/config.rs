86a9e42d68d24c4208a2b585f85b3aec2d623ac3
Trent Nicholson <nicholsontrent@gmail.com>
2024-11-28 18:50:17 +1300

    chore!: Simplify config and profile list command implementation

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -14,12 +14,10 @@ pub fn list(config: &DotsyConfig) {
     let configs_regex = &config
         .dotfiles
         .join(&config.configs_dir)
-        .join("*.config.json")
-        .into_os_string()
-        .to_owned();
-    let configs_found =
-        glob(&configs_regex.to_str().unwrap()).expect("Failed to read glob pattern");
-    let configs = configs_found.into_iter().peekable();
+        .join("*.config.json");
+
+    let configs = glob(configs_regex.to_str().unwrap()).expect("Failed to read glob pattern");
+
     println!("Available Configs to install");
     configs.for_each(|e| {
         println!(" - {}", e.unwrap().display());
--- src/commands/profile.rs
+++ src/commands/profile.rs
@@ -18,12 +18,10 @@ pub fn list(config: &DotsyConfig) {
     let profiles_regex = &config
         .dotfiles
         .join(&config.profiles_dir)
-        .join("*.profile.json")
-        .into_os_string()
-        .to_owned();
-    let profiles_found =
-        glob(&profiles_regex.to_str().unwrap()).expect("Failed to read glob pattern");
-    let profiles = profiles_found.into_iter().peekable();
+        .join("*.profile.json");
+
+    let profiles = glob(profiles_regex.to_str().unwrap()).expect("Failed to read glob pattern");
+
     println!("Available Profiles to install");
     profiles.for_each(|e| {
         println!(" - {}", e.unwrap().display());
